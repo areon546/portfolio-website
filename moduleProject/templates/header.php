ef02009227859456f90f9deed3b50c91db2a0520
Areon546 <57325743+areon546@users.noreply.github.com>
2024-03-20 12:06:32 +0000

    I am now importing variables in SASS, added a template folder to eventually import templates from, and made some more files to evenutally have stuff on

---
+++ moduleProject/templates/header.php
@@ -0,0 +1,10 @@
+<header class="header">
+        <nav>
+            <a href="index.html">Artur Baran</a>
+
+            <ul>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="blog.html">Blog</a></li>
+            </ul>
+        </nav>
+</header>
